3e049bc7b52e0eccad05eec6adf2dde506836453
mikko.nieminen <mikko.nieminen@bihealth.de>
2018-10-19 14:27:28 +0200

    add tests for RemoteProjectGetAPIView

--- projectroles/tests/test_views.py
+++ projectroles/tests/test_views.py
@@ -1,6 +1,7 @@
 """Tests for views in the projectroles Django app"""
 
 import base64
+import json
 from urllib.parse import urlencode
 
 from django.conf import settings
@@ -17,6 +18,7 @@ from ..models import Project, Role, RoleAssignment, ProjectInvite, \
     PROJECT_TAG_STARRED
 from ..plugins import change_plugin_status, get_backend_api, \
     get_active_plugins, ProjectAppPluginPoint
+from ..remote_projects import RemoteProjectAPI
 from ..utils import build_secret
 from .test_models import ProjectMixin, RoleAssignmentMixin, \
     ProjectInviteMixin, ProjectUserTagMixin, RemoteSiteMixin, RemoteProjectMixin
@@ -1962,3 +1964,64 @@ class TestRemoteProjectsBatchUpdateView(
                 reverse(
                     'projectroles:remote_projects',
                     kwargs={'remotesite': self.target_site.sodar_uuid}))
+
+
+class TestRemoteProjectGetAPIView(
+        TestViewsBase, ProjectMixin, RoleAssignmentMixin, RemoteSiteMixin,
+        RemoteProjectMixin):
+    """Tests for remote project getting API view"""
+
+    def setUp(self):
+        super(TestRemoteProjectGetAPIView, self).setUp()
+
+        # Set up projects
+        self.category = self._make_project(
+            'TestCategory', PROJECT_TYPE_CATEGORY, None)
+        self.cat_owner_as = self._make_assignment(
+            self.category, self.user, self.role_owner)
+        self.project = self._make_project(
+            'TestProject', PROJECT_TYPE_PROJECT, self.category)
+        self.project_owner_as = self._make_assignment(
+            self.project, self.user, self.role_owner)
+
+        # Create target site
+        self.target_site = self._make_site(
+            name=REMOTE_SITE_NAME,
+            url=REMOTE_SITE_URL,
+            mode=SITE_MODE_TARGET,
+            description=REMOTE_SITE_DESC,
+            secret=REMOTE_SITE_SECRET)
+
+        # Create remote project
+        self.remote_project = self._make_remote_project(
+            site=self.target_site,
+            project_uuid=self.project.sodar_uuid,
+            project=self.project,
+            level=SODAR_CONSTANTS['REMOTE_LEVEL_READ_INFO'])
+
+        self.remote_api = RemoteProjectAPI()
+
+    def test_get(self):
+        """Test retrieving project data to the target site"""
+
+        response = self.client.get(
+            reverse(
+                'projectroles:api_remote_get',
+                kwargs={'secret': REMOTE_SITE_SECRET}))
+
+        self.assertEqual(response.status_code, 200)
+
+        expected = self.remote_api.get_target_data(self.target_site)
+        response_dict = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual(response_dict, expected)
+
+    def test_get_invalid_secret(self):
+        """Test retrieving project data with an invalid secret (should fail)"""
+
+        response = self.client.get(
+            reverse(
+                'projectroles:api_remote_get',
+                kwargs={'secret': build_secret()}))
+
+        self.assertEqual(response.status_code, 401)
