68e7c7b9f7bcbc6caafc7b24ec2bfc2ffd81fb2f
mikko.nieminen <mikko.nieminen@bihealth.de>
2018-10-23 11:48:43 +0200

    add missing doc reference in user model comments

--- example_site/users/models.py
+++ example_site/users/models.py
@@ -7,7 +7,7 @@ from projectroles.models import SODARUser
 
 # NOTE: If integrating projectroles with an existing project, you will have to
 #       include migration scripts for populating the sodar_uuid field. See:
-#
+#       https://docs.djangoproject.com/en/1.11/howto/writing-migrations/#migrations-that-add-unique-fields
 
 
 class User(SODARUser):
