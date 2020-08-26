74567ddd9d6b7af9e30568ff0ed392e36666b92d
mikko.nieminen <mikko.nieminen@bihealth.de>
2020-08-26 11:57:19 +0200

    fix install_chrome.sh

--- utility/install_chrome.sh
+++ utility/install_chrome.sh
@@ -12,10 +12,10 @@ sudo apt-get update
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
 
 # Install Chrome
-# wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/
-# sudo dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
-# sudo apt-get -f install -y
-# rm ~/google-chrome-stable_current_amd64.deb
+wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/
+sudo dpkg -i --force-depends ~/google-chrome-stable_current_amd64.deb
+sudo apt-get -f install -y
+rm ~/google-chrome-stable_current_amd64.deb
 
 # Install ChromeDriver
 wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
